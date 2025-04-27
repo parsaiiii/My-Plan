c1e82ea825ffe6af36bd5b63ea32d6afd30af743
Parsa.code <Parsa.code.@gmile>
2025-04-27 15:49:54 +0330

    a

---
+++ src/components/UserListTes.vue
@@ -0,0 +1,42 @@
+<template>
+  <button @click="grtUser">run</button>
+  <div>
+    <h2>user list</h2>
+    <p v-if="isLodeng">loading ...</p>
+    <p v-if="error">{{ error }}</p>
+    <ul v-show="!isLodeng && !error">
+      <li v-for="user in usersr" :key="user.id">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "../api";
+
+export default {
+  data() {
+    return {
+      usersrr: [],
+      isLodeng: true,
+      error: null,
+      getU: "",
+    };
+  },
+  methods: {
+    async grtUser() {
+      try {
+        this.getU = await axios.get("/users");
+        this.usersr = this.getU.data;
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.isLodeng = false;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,44 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHashHistory } from "vue-router";
+// import HomeViewTest from "../views/HomeViewTest.vue";
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+  // {
+  //   path: "/a",
+  //   component: HomeViewTest,
+  //   children:[
+  //     {
+  //       path:'/profile',component:AboutView
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "/home",
+  //   name: "HomeViewTest",
+  //   component: () =>
+  //     import(
+  //       /* webpackChunkName: "HomeViewTest" */ "../views/HomeViewTest.vue"
+  //     ),
+  //   children: [
+  //     {
+  //       path: "/profile",
+  //       name: "AboutView",
+  //       component: () =>
+  //         import(/* webpackChunkName: "A" */ "../views/AboutView.vue"),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
